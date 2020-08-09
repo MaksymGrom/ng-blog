fba23e65fdaa369b5e4782faf46e94a018aed1d0
Grom Max <MailMaximGrom@gmail.com>
2020-08-09 11:51:26 +0300

    #24 JWT расшифрован согласно https://github.com/MaksymGrom/ng-blog/issues/24

--- src/app/store/admin-auth-store/admin-auth-store.module.ts
+++ src/app/store/admin-auth-store/admin-auth-store.module.ts
@@ -5,12 +5,18 @@ import {ADMIN_AUTH_FEATURE_NAME, adminAuthReducer} from './store/admin-auth.redu
 import {HttpClientModule} from '@angular/common/http';
 import {EffectsModule} from '@ngrx/effects';
 import {AdminAuthEffects} from './store/admin-auth.effects';
+import {JwtModule} from '@auth0/angular-jwt';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: request => request as any
+      }
+    }),
     StoreModule.forFeature(
       ADMIN_AUTH_FEATURE_NAME,
       adminAuthReducer
--- src/app/store/admin-auth-store/services/admin-auth.service.ts
+++ src/app/store/admin-auth-store/services/admin-auth.service.ts
@@ -1,18 +1,28 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {JwtHelperService} from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminAuthService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private jwtHelperService: JwtHelperService
+  ) { }
 
   login(body: {login: string, password: string}) {
     return this.httpClient.post<{accessToken: string}>(
       'http://localhost:3000/auth/login',
       body
+    ).pipe(
+      map(res => ({
+        ...res,
+        ...this.jwtHelperService.decodeToken(res.accessToken)
+      }))
     );
   }
 }
