592c7f5a40092e4d01d4bbb6c669e10b677752c8
Grom Max <MailMaximGrom@gmail.com>
2020-08-16 16:08:06 +0300

    #30 добавлен функционал востановления токена

--- src/app/store/admin-auth-store/store/admin-auth.effects.ts
+++ src/app/store/admin-auth-store/store/admin-auth.effects.ts
@@ -1,9 +1,12 @@
 import {Injectable} from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import {login, loginFailed, loginSuccess} from './admin-auth.actions';
-import {catchError, map, switchMap} from 'rxjs/operators';
+import {catchError, delay, delayWhen, filter, first, map, switchMap} from 'rxjs/operators';
 import {AdminAuthService} from '../services/admin-auth.service';
-import {of} from 'rxjs';
+import {of, timer} from 'rxjs';
+import {AuthData} from './admin-auth.reducer';
+import {select, Store} from '@ngrx/store';
+import {isAuth} from './admin-auth.selectors';
 
 @Injectable()
 export class AdminAuthEffects {
@@ -14,7 +17,7 @@ export class AdminAuthEffects {
       login: action.login,
       password: action.password
     }).pipe(
-      map(loginSuccessData => loginSuccess(loginSuccessData)),
+      map((loginSuccessData: AuthData) => loginSuccess(loginSuccessData)),
       catchError(
         error => of(loginFailed({
           serverError: error.message
@@ -23,8 +26,26 @@ export class AdminAuthEffects {
     ))
   ));
 
+  refresh$ = createEffect(() => this.actions$.pipe(
+    ofType(loginSuccess),
+    delayWhen(
+      (action: AuthData) => timer(
+        action.exp * 1000 - 60 * 1000 - Date.now()
+      )
+    ),
+    switchMap(() => this.store$.pipe(
+      select(isAuth),
+      first(),
+      filter(isAdminAuth => isAdminAuth)
+    )),
+    switchMap(() => this.adminAuthService.refresh().pipe(
+      map((loginSuccessData: AuthData) => loginSuccess(loginSuccessData))
+    ))
+  ));
+
   constructor(
     private actions$: Actions,
-    private adminAuthService: AdminAuthService
+    private adminAuthService: AdminAuthService,
+    private store$: Store
   ) { }
 }
